b734a524e077b664cb27230a040b4a2d2ce9c79d
Rashad Davis <rashaddavis@Rashads-MacBook-Pro.local>
2020-02-11 17:22:02 -0600

    almost donw

--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
 var addSkill = $('#skill')
-// var addLi = 
+var deleteButton = $()
 
  $('button').on('click', function(){
     var $addLi = $(`<li>${addSkill.val()}</li>`) 
     $('ul').append($addLi);
+    $('li').remove($addLi);
 });
+
+
 
 
